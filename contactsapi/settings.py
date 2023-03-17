c3105d76b4b78e0e38eb0b795ba813606a0062b9
Emmanuel Olabayode <emmanuelolabayode@gmail.com>
2023-03-17 14:46:41 +0000

    ch(deploy): setup deploy to heroku

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+import django_heroku
+
 
 
 load_dotenv()
@@ -150,3 +152,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+django_heroku.settings(locals())
